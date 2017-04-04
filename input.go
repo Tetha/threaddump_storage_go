3878e3d3622922d8ce9ba4ee9621e1a63c7d5b98
Harald Kraemer <tetha42@gmail.com>
2017-04-04 21:40:11 +0200

    implemented current

--- input.go
+++ input.go
@@ -1,13 +1,17 @@
 package input
 
-type input struct {
+type Input struct {
     content string;
     position int;
     marks []int;
 }
 
-func CreateInput(content string) (r input) {
-    r = input{content: content, position: 0}
+func CreateInput(content string) (r Input) {
+    r = Input{content: content, position: 0}
     r.marks = make([]int, 10)
     return
+}
+
+func (input *Input) Current() byte {
+    return input.content[input.position]
 }
--- input_test.go
+++ input_test.go
@@ -12,3 +12,10 @@ func TestCreateInput(t *testing.T) {
         t.Errorf("Expected position to be 0, but was %d", result.position)
     }
 }
+
+func TestCurrent(t *testing.T) {
+    result := CreateInput("someString")
+    if result.Current() != "s"[0] {
+        t.Errorf("Expected current char to be <s>, but was %s", result.content)
+    }
+}
