5ae90d4379cd1f4f53647f9ebb989b0d02adb1b5
Harald Kraemer <tetha42@gmail.com>
2017-04-15 20:53:35 +0200

    Handled early failure

--- parser_primitives_test.go
+++ parser_primitives_test.go
@@ -13,3 +13,15 @@ func TestMatchString(t *testing.T) {
 		t.Errorf("MatchWord should advance input to <S> but was %q", parser.Current())
 	}
 }
+
+func TestMatchStringFailsInWord(t *testing.T) {
+	parser := CreateInput("someString")
+	matched := parser.MatchWord("somd")
+	if matched {
+		t.Error("MatchWord didn't fail")
+	}
+
+	if parser.Current() != 's' {
+		t.Errorf("MatchWord didn't roll back properly, and got stuck on %q",  parser.Current())
+	}
+}
