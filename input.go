3dfc3de5f9b81fcb60ef8dcd1b72fcc3706f74a1
Harald Kraemer <tetha42@gmail.com>
2017-04-15 19:16:33 +0200

    go fmt

--- input.go
+++ input.go
@@ -4,37 +4,37 @@ import "errors"
 import "unicode/utf8"
 
 type Input struct {
-    content string;
-    position int;
-    marks []int;
+	content  string
+	position int
+	marks    []int
 }
 
 func CreateInput(content string) (r Input) {
-    r = Input{content: content, position: 0}
-    r.marks = make([]int, 0, 10)
-    return
+	r = Input{content: content, position: 0}
+	r.marks = make([]int, 0, 10)
+	return
 }
 
 func (input *Input) Current() rune {
-    runeValue, _ := utf8.DecodeRuneInString(input.content[input.position:])
-    return runeValue
+	runeValue, _ := utf8.DecodeRuneInString(input.content[input.position:])
+	return runeValue
 }
 
 func (input *Input) Advance() {
-    _, width := utf8.DecodeRuneInString(input.content[input.position:])
-    input.position += width
+	_, width := utf8.DecodeRuneInString(input.content[input.position:])
+	input.position += width
 }
 
 func (input *Input) Mark() {
-    input.marks = append(input.marks, input.position)
+	input.marks = append(input.marks, input.position)
 }
 
 func (input *Input) Rollback() error {
-    if len(input.marks) == 0 {
-        return errors.New("No previous mark!")
-    }
-    lastPosition := -1
-    lastPosition, input.marks = input.marks[len(input.marks)-1], input.marks[:len(input.marks)-1]
-    input.position = lastPosition
-    return nil
+	if len(input.marks) == 0 {
+		return errors.New("No previous mark!")
+	}
+	lastPosition := -1
+	lastPosition, input.marks = input.marks[len(input.marks)-1], input.marks[:len(input.marks)-1]
+	input.position = lastPosition
+	return nil
 }
