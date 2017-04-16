90a53c452ff7d19d8e35dd757386b70b4a8b7be9
Harald Kraemer <tetha42@gmail.com>
2017-04-16 14:56:52 +0200

    Added proper rollbacks to bloked line parser

--- blocked_line_parser.go
+++ blocked_line_parser.go
@@ -4,28 +4,35 @@ func (input *Input) ParseBlockedLine() (bool, StacktraceLine) {
 	var result StacktraceLine
 	var parsed = false
 
+	input.Mark()
 	if !input.MatchWord("\t- parking to wait for ") {
+		input.Rollback()
 		return false, result
 	}
 
 	parsed, result.LockAddress = input.DelimitedWord('<', '>')
 	if !parsed {
+		input.Rollback()
 		return false, result
 	}
 
 	if !input.MatchWord(" (a ") {
+		input.Rollback()
 		return false, result
 	}
 
 	parsed, result.LockClass = input.ReadUntil(')')
 	if !parsed {
+		input.Rollback()
 		return false, result
 	}
 
 	if !input.MatchWord(")\n") {
+		input.Rollback()
 		return false, result
 	}
 
+	input.Commit()
 	result.Type = BlockedLine
 	return true, result
 }
