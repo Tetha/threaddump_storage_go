d8a9c1ea695273e06f76b4558c0d0ff8ac33eab0
Harald Kraemer <tetha42@gmail.com>
2017-04-16 11:04:47 +0200

    Happy case delimited word working

--- delimited_word.go
+++ delimited_word.go
@@ -1,6 +1,18 @@
 
 package input
 
+import "bytes"
+
 func (input *Input) DelimitedWord(start rune, end rune) (bool, string) {
-	return false, ""
+	if input.Current() != start {
+		return false, ""
+	}
+	var word bytes.Buffer
+	input.Advance()
+	for input.Current() != end {
+		word.WriteRune(input.Current())
+		input.Advance()
+	}
+	input.Advance()
+	return true, word.String()
 }
--- delimited_word_test.go
+++ delimited_word_test.go
@@ -9,3 +9,17 @@ func TestDelimitedWordWithoutStartingDelimiter(t *testing.T) {
 		t.Errorf("DelimitedWord should not parse, but parsed %s", delimitedWord)
 	}
 }
+
+func TestDelimitedWordWithDelimitedWord(t *testing.T) {
+	parser := CreateInput("<someString>$")
+	parsed, delimitedWord := parser.DelimitedWord('<', '>')
+	if !parsed {
+		t.Error("DelimitedWord didn't parse properly")
+	}
+	if delimitedWord != "someString" {
+		t.Errorf("DelimitedWord should have parsed <someString>, but parsed <%s>", delimitedWord)
+	}
+	if parser.Current() != '$' {
+		t.Errorf("DelimitedWord should advance the input past the closing delimiter, but only advanced to %q", parser.Current())
+	}
+}
