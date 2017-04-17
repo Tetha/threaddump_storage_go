f121aec0f5c3c87e5b35c19603e8aea398ee9117
Harald Kraemer <tetha42@gmail.com>
2017-04-17 15:13:35 +0200

    Simplified code with defer

--- blocked_line_parser.go
+++ blocked_line_parser.go
@@ -4,36 +4,39 @@ func (input *Input) ParseBlockedLine() (success bool, result StacktraceLine) {
 	var parsed = false
 	success = false
 
-	result.Type = BlockedLine
 	input.Mark()
+	defer func() {
+		if success {
+			input.Commit()
+		} else {
+			input.Rollback()
+		}
+	}()
+
+	result.Type = BlockedLine
+
 	if !(input.MatchWord("\t- parking to wait for ") || input.MatchWord("\t- waiting to lock ")) {
-		input.Rollback()
 		return
 	}
 
 	parsed, result.LockAddress = input.DelimitedWord('<', '>')
 	if !parsed {
-		input.Rollback()
 		return
 	}
 
 	if !input.MatchWord(" (a ") {
-		input.Rollback()
 		return
 	}
 
 	parsed, result.Class = input.ReadUntil(')')
 	if !parsed {
-		input.Rollback()
 		return
 	}
 
 	if !input.MatchWord(")\n") {
-		input.Rollback()
 		return
 	}
 
-	input.Commit()
 	success = true
 	return
 }
