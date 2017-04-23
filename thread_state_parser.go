b6eba645e6462a10795e2a89508b821f604d0269
Harald Kraemer <tetha42@gmail.com>
2017-04-23 10:19:25 +0200

    Handle simple runnable states

--- thread_state_parser.go
+++ thread_state_parser.go
@@ -1,5 +1,21 @@
 package input
 
 func (input *Input) ParseThreadState() (success bool, state string, clarification string) {
+	parsed := false
+
+	if !input.MatchWord("   java.lang.Thread.State: ") {
+		return
+	}
+
+	parsed, state = input.ReadUntil('\n')
+	if !parsed {
+		return
+	}
+
+	if !input.MatchWord("\n") {
+		return
+	}
+
+	success = true
 	return
 }
