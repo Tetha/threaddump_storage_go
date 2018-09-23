440881f2bb53a37836e6a4fa0a4b359c246903c7
Harald Kraemer <tetha42@gmail.com>
2018-09-23 19:31:02 +0200

    Rework parked line parser test to betablebased

--- input/parked_line_parser_test.go
+++ input/parked_line_parser_test.go
@@ -2,27 +2,21 @@ package input
 
 import "testing"
 
+var parkedLineTests = []lineParserTest{
+	{
+		"\t- parking to wait for  <0x000000071fb23b18> (a java.util.concurrent.SynchronousQueue$TransferStack)\n$",
+		true,
+		StacktraceLine{
+			Type:        ParkedLine,
+			LockAddress: "0x000000071fb23b18",
+			Class:       "java.util.concurrent.SynchronousQueue$TransferStack",
+		},
+		'$',
+	},
+}
+
 func TestParseParkedLine(t *testing.T) {
-	parser := CreateInput("\t- parking to wait for  <0x000000071fb23b18> (a java.util.concurrent.SynchronousQueue$TransferStack)\n$")
-	parsed, line := parser.ParseParkedLine()
-
-	if !parsed {
-		t.Error("ParseLockedLine should succeed on valid inputs")
-	}
-
-	if line.Type != ParkedLine {
-		t.Errorf("Expected result lock line to be ParkedLine (%d), but got %d", ParkedLine, line.Type)
-	}
-
-	if line.LockAddress != "0x000000071fb23b18" {
-		t.Errorf("Expected ParseLockedLine to extract LockAddress: <0x00000000e0e97cb0>, but got <%s>", line.LockAddress)
-	}
-
-	if line.Class != "java.util.concurrent.SynchronousQueue$TransferStack" {
-		t.Errorf("Expeted ParseLockedLine to extract Class: java.util.concurrent.SynchronousQueue$TransferStack, but got <%s>", line.Class)
-	}
-
-	if parser.Current() != '$' {
-		t.Errorf("Expected input to be placed on the next line but it got stuck on <%q>", parser.Current())
+	for idx, tt := range parkedLineTests {
+		runParserTestcase(t, idx, tt, (*Input).ParseParkedLine)
 	}
 }
