d949dfd580c81b6f669817f7b12c2462d89065cc
Harald Kraemer <tetha42@gmail.com>
2018-09-23 20:27:57 +0200

    Convert thread state parser test to table

--- input/thread_state_parser_test.go
+++ input/thread_state_parser_test.go
@@ -6,24 +6,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseThreadStateRunning(t *testing.T) {
-	parser := CreateInput("   java.lang.Thread.State: RUNNABLE\n$")
-	parsed, state, clarification := parser.ParseThreadState()
+var threadStateTests = map[string]struct {
+	input string
 
-	assert := assert.New(t)
-	assert.True(parsed, "ParseThreadState should succeed on valid input")
-	assert.Equal("RUNNABLE", state)
-	assert.Equal("", clarification, "Runnable has no clarification")
-	assert.Equal(byte('$'), parser.Current(), "ParseThreadState should consume the entire input")
+	shouldParse   bool
+	state         string
+	clarification string
+	current       byte
+}{
+	"no clarification": {
+		"   java.lang.Thread.State: RUNNABLE\n$",
+		true,
+		"RUNNABLE",
+		"",
+		'$',
+	},
+	"clarification": {
+		"   java.lang.Thread.State: WAITING (parking)\n$",
+		true,
+		"WAITING",
+		"parking",
+		'$',
+	},
 }
 
-func TestParseThreadStateWaiting(t *testing.T) {
-	parser := CreateInput("   java.lang.Thread.State: WAITING (parking)\n$")
-	parsed, state, clarification := parser.ParseThreadState()
+func TestParseThreadState(t *testing.T) {
+	for description, tt := range threadStateTests {
+		parser := CreateInput(tt.input)
+		parsed, state, clarification := parser.ParseThreadState()
 
-	assert := assert.New(t)
-	assert.True(parsed, "ParseThreadState should succeed on valid input")
-	assert.Equal("WAITING", state)
-	assert.Equal("parking", clarification, "Runnable has no clarification")
-	assert.Equal(byte('$'), parser.Current(), "ParseThreadState should consume the entire input")
+		assert := assert.New(t)
+		assert.Equal(tt.shouldParse, parsed, description)
+		assert.Equal(tt.state, state, description)
+		assert.Equal(tt.clarification, clarification, description)
+		assert.Equal(tt.current, parser.Current(), description)
+	}
 }
