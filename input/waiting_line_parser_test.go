51026811fcb66e5ea177dbe2eeefab02aa200063
Harald Kraemer <tetha42@gmail.com>
2018-10-01 20:38:50 +0200

    Convert waiting line parser to table test

--- input/waiting_line_parser_test.go
+++ input/waiting_line_parser_test.go
@@ -2,22 +2,21 @@ package input
 
 import "testing"
 
+var waitLineTests = []lineParserTest{
+	{
+		"\t- waiting on <0x0000000729ffa7f8> (a java.lang.ref.ReferenceQueue$Lock)\n$",
+		true,
+		StacktraceLine{
+			Type:        WaitingLine,
+			LockAddress: "0x0000000729ffa7f8",
+			Class:       "java.lang.ref.ReferenceQueue$Lock",
+		},
+		'$',
+	},
+}
+
 func TestParseWaitLine(t *testing.T) {
-	parser := CreateInput("\t- waiting on <0x0000000729ffa7f8> (a java.lang.ref.ReferenceQueue$Lock)\n$")
-	parsed, line := parser.ParseWaitLine()
-	if !parsed {
-		t.Error("ParseWaitLine didn't succeed on valid input")
-	}
-
-	if line.Type != WaitingLine {
-		t.Errorf("ParseWaitline should have returned a WaitingLine (%d), but created a %d", WaitingLine, line.Type)
-	}
-
-	if line.LockAddress != "0x0000000729ffa7f8" {
-		t.Errorf("Lock address wasn't extracted properly, got: <%s>", line.LockAddress)
-	}
-
-	if line.Class != "java.lang.ref.ReferenceQueue$Lock" {
-		t.Errorf("Lock class wasn't extracted properly, got: <%s>", line.Class)
+	for idx, tt := range waitLineTests {
+		runParserTestcase(t, idx, tt, (*Input).ParseWaitLine)
 	}
 }
