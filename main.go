6c9504ffe52a17a01c05eebb3c5894352b7f931e
Harald Kraemer <tetha42@gmail.com>
2018-12-08 20:21:45 +0100

    Removed more code

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
-
-	"html/template"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/tetha/threaddumpstorage-go/database"
@@ -16,44 +13,6 @@ import (
 	"net/http"
 	"net/http/pprof"
 )
-
-type Threaddump struct {
-	ID          int       `json:"id"`
-	Application string    `json:"application"`
-	Host        string    `json:"host"`
-	Uploaded    time.Time `json:"uploaded"`
-}
-
-type JavaThread struct {
-	Id                     int              `json:"id"`
-	Name                   string           `json:"name"`
-	JavaId                 string           `json:"java_id"`
-	IsDaemon               bool             `json:"is_daemon"`
-	Prio                   int              `json:"prio"`
-	OsPrio                 int              `json:"os_prio"`
-	Tid                    string           `json:"tid"`
-	Nid                    string           `json:"nid"`
-	NativeThreadState      string           `json:"native_thread_state"`
-	ConditionAddress       string           `json:"condition_address"`
-	JavaThreadState        string           `json:"java_thread_state"`
-	JavaStateClarification string           `json:"java_state_clarification"`
-	StacktraceLines        []StacktraceLine `json:"stacktrace_lines"`
-}
-
-type StacktraceLine struct {
-	Id          int     `json:"id"`
-	Kind        string  `json:"kind"`
-	LineNumber  int     `json:"line_number"`
-	LockAddress *string `json:"lock_address,omitempty"`
-	LockedClass *string `json:"locked_class,omitempty"`
-	LockClass   *string `json:"lock_class,omitempty"`
-	JavaClass   *string `json:"java_class,omitempty"`
-	JavaMethod  *string `json:"java_method,omitempty"`
-	SourceLine  *string `json:"source_line,omitempty"`
-	SourceFile  *string `json:"source_file,omitempty"`
-}
-
-var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	r := mux.NewRouter()
